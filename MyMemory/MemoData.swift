16a44d93e3492da29a56a16464f0a6b16c906168
CIH <jihktwy@gmail.com>
2019-09-06 17:00:02 +0900

    메모 기본기능 완성

---
+++ MyMemory/MemoData.swift
@@ -0,0 +1,17 @@
+//
+//  MemoData.swift
+//  MyMemory
+//
+//  Created by CIH-Imac on 06/09/2019.
+//  Copyright © 2019 CIH. All rights reserved.
+//
+
+import UIKit
+
+class MemoData {
+    var memoIdx: Int?
+    var title: String?
+    var contents: String?
+    var image: UIImage?
+    var regdate: Date?
+}
--- MyMemory/MemoFormVC.swift
+++ MyMemory/MemoFormVC.swift
@@ -9,11 +9,37 @@
 import UIKit
 
 class MemoFormVC: UIViewController {
-
+    var subject: String!
+    
+    @IBOutlet var contents: UITextView!
+    @IBOutlet var preview: UIImageView!
+    
+    @IBAction func save(_ sender: Any) {
+        // 내용을 입력하지 않았을 경우, 경고한다.
+        guard self.contents.text?.isEmpty == false else {
+            let alert = UIAlertController(title: nil, message: "내용을 입력해주세요", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        // MemoData 객체를 생성하고 데이터를 담는다.
+        let data = MemoData()
+        data.title = self.subject
+        data.contents = self.contents.text
+        data.image = self.preview.image
+        data.regdate = Date()
+        
+        // 앱 델리게이트 객체를 읽어온 다음 memolist 배열에 MemoData 객체를 추가
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.memolist.append(data)
+        
+        // 작성폼 화면을 종료하고 이전화면으로 돌아간다.
+        self.navigationController?.popViewController(animated: true)
+    }
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.contents.delegate = self
     }
     
 
@@ -27,4 +53,32 @@ class MemoFormVC: UIViewController {
     }
     */
 
+}
+
+extension MemoFormVC: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBAction func pick(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        self.present(picker, animated: false)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let editedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage{
+            self.preview.image = editedImage
+            picker.dismiss(animated: false)
+        }
+    }
+}
+
+extension MemoFormVC: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        // 내용의 최대 15자리까지 읽어 subject 변수에 저장한다.
+        let contents = textView.text as NSString
+        let length = ((contents.length > 15) ? 15 : contents.length)
+        self.subject = contents.substring(with: NSRange(location: 0, length: length))
+        // 네비게이션 타이틀에 표시한다.
+        self.navigationItem.title = subject
+    }
 }
