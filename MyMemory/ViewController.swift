23989ee76d89f5c68e374e8d55f3790e5366d1f7
CIH <jihktwy@gmail.com>
2019-09-06 14:09:28 +0900

    첫 커밋

---
+++ MyMemory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMemory
+//
+//  Created by CIH-Imac on 06/09/2019.
+//  Copyright © 2019 CIH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
