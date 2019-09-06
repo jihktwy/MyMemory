91b126b21d3e77f101c0a053a6394dc6db7e994b
CIH <jihktwy@gmail.com>
2019-09-06 15:21:39 +0900

    뷰 작성, 클래스 연결

---
+++ MyMemory/MemoCell.swift
@@ -0,0 +1,24 @@
+//
+//  MemoCell.swift
+//  MyMemory
+//
+//  Created by CIH-Imac on 06/09/2019.
+//  Copyright © 2019 CIH. All rights reserved.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
